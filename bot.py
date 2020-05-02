967fef9024be91265dc23fd53901b4b63944fa63
diduchenkor <diduchenkoru@gmail.com>
2020-05-02 18:40:13 +0300

    images and keyboards

--- bot.py
+++ bot.py
@@ -1,65 +1,94 @@
-#Для начяла нужно импортировать фали с помощю апдейтера Updater с библеотеки ###
-#Updater соединяе мене з айпишкой телеги#
-#Для того чтобы бот реагирувал на команду старт имопортируеться соманд хендлер
-#Для отчета добавляем ипорт логинг - встроенний в пайтон
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from emoji import emojize
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, RegexHandler, Filters
+
+from glob import glob
 import logging
+from random import choice
+
 import settings
 
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
-                    filename="bot.log"
+                    filename='bot.log'
                     )
 
-#Добавляем код на githab
+def greet_user(bot, update, user_data):
+    emo = get_user_emo(user_data)
+    user_data['emo'] = emo
+    text ='Привет  {}'.format(emo)
+
+    update.message.reply_text(text, reply_markup=get_keyboard())
 
 
-# обработчик 
-def greet_user(bot, update):
-    #Виводить текст в командрій строци 
-    text ='Визов/start'
-    print(text)
-    
-    #отвичяе пользователю
-    update.messrge.reply_text(text)
-
-
-# создаеться переменная с названием юзер текст в которую кладется то что пришло з абдейт юзер текс,сообщение прльзоватиля. и пичятаеться в консоль це принт. 3 команда отпраляе то что нам прислал юзер
-def talk_to_me(bot, update):
-    user_text = "Привет {}! Ты написал {}".format(update.message.chat.first_name, update.message.text)
-    
+def talk_to_me(bot, update, user_data):
+    emo = get_user_emo(user_data)
+    user_text = "Привет {} {}! Ты написал {}".format(update.message.chat.first_name, emo, 
+                        update.message.text) 
     logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username,
                 update.message.chat.id, update.message.text)
+    update.message.reply_text(user_text, reply_markup=get_keyboard())
 
-    update.message.reply_text(user_text)
-
-
-
-#Тело бота це функция ии можна назвать мейн, запускалка нашого бота 
-def main():
-    #дальше переменна # Ключик бота 
-    mybot = Updater(settings.API_KEY)  
+def send_cat_picture(bot, update, user_data):
+    cat_list = glob('images/cat*.jpg')
+    cat_pic = choice(cat_list)
+    bot.send_photo(chat_id=update.message.chat.id, photo=open(cat_pic, 'rb'), reply_markup=get_keyboard())
     
-    #Добавляем функицию яка передае инфо в наши логи
-    logging.info("bot star")
+def change_avatar(bot, update, user_data):
+    if'emo' in user_data:
+        del user_data['emo']
+        emo = get_user_emo(user_data)
+        update.message.reply_text('Готово {}'.format(emo), reply_markup=get_keyboard())
 
-     #Переводим  в переменную для легкость читания
-     #реагируе на оосбщение входящи о отсилае на получятелей 
+
+def get_user_emo(user_data):
+    if 'emo' in user_data:
+        return user_data['emo']
+    else:
+        user_data['emo'] = emojize(choice(settings.USER_EMOJI), use_aliases=True)
+        return user_data['emo']
+
+def get_contact(bot, update, user_data):
+    print(update.message.contact)
+    update.message.reply_text('Готово: {}'.format(get_user_emo(user_data)), reply_markup=get_keyboard())
+
+def get_location(bot, update, user_data):
+    print(update.message.location)
+    update.message.reply_text('Готово: {}'.format(get_user_emo(user_data)), reply_markup=get_keyboard())
+
+
+def get_keyboard():
+    contact_button = KeyboardButton('Прислать контакт', request_contact=True)
+    location_button = KeyboardButton('Прислать координаты', request_location=True)
+    my_keyboard = ReplyKeyboardMarkup([
+                                        ['Прислать фотку', 'Сменить аватарку'],
+                                        [ contact_button,   location_button ]
+                                     ], resize_keyboard=True
+                                    )
+    return my_keyboard
+
+def main():
+    mybot = Updater(settings.API_KEY)  
+    logging.info('Бот запускается')
     dp = mybot.dispatcher 
-    dp.add_handler(CommandHandler("start", greet_user))
-    #данний код реагируе на тексови сообщения и звязаний з функицей ток туми
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+   
+    dp.add_handler(CommandHandler('start', greet_user, pass_user_data=True))
+  
+    dp.add_handler(CommandHandler('cat', send_cat_picture, pass_user_data=True))
+   
+    dp.add_handler(RegexHandler('^(Прислать фотку)$', send_cat_picture, pass_user_data=True))
 
-    # це значить начни ходить на платформу телеграм и провиряй наличие сообщений
+    dp.add_handler(RegexHandler('^(Сменить аватарку)$', change_avatar, pass_user_data=True))
+
+    dp.add_handler(MessageHandler(Filters.contact, get_contact, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.location, get_contact, pass_user_data=True))
+
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
+  
     mybot.start_polling()
-
-    #це ознаяе що бот будет работать пока принудительно його не остановиш
     mybot.idle()
 
 
 
-
-
-#тепер пишельтя опридиленный код який буду при визови роботать
 main()
